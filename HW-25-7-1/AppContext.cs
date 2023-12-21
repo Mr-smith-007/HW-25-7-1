e7f1cb09d588c04ade83be75ae2cebbb95a90b7e
Mr-Smith-007 <mr_smith_007@mail.ru>
2023-12-21 10:10:08 +0300

    Добавьте файлы проекта.

---
+++ HW-25-7-1/AppContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_25_7_1
+{
+    internal class AppContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        public AppContext() 
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=HW-25-7-1;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+    }
+}
---
+++ HW-25-7-1/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+
+namespace HW_25_7_1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
